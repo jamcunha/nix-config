02cf80f4162f4fe6636ef7ebb05d052c5c4de6c2
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2024-07-20 20:26:40 +0100

    started work with new config structure

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,146 +1,25 @@
-{ inputs, config, lib, pkgs, ... }:
-
-{
-  imports = [
-    inputs.disko.nixosModules.disko
-    ./disko.nix
-
-    inputs.hardware.nixosModules.common-cpu-intel-comet-lake
-    inputs.hardware.nixosModules.common-gpu-nvidia-disable
-    inputs.hardware.nixosModules.common-pc-ssd
-    ./hardware-configuration.nix
-
-    ./users/afonso.nix
-
-    # TODO: organize this files to be common to all systems
-    ./pipewire.nix
-    ./nix.nix
-  ];
-
-  # system globals
-  username = "afonso";
-  hostname = "laptop";
-  fullName = "Joaquim Cunha";
-  gitName = "Joaquim Cunha";
-  gitEmail = "joaquimafonsocunha@gmail.com";
-
-  # temp fix for temperature
-  powerManagement.cpuFreqGovernor = "powersave";
-
-  networking.hostName = config.hostname;
-  networking.networkmanager.enable = true;
-
-  time.timeZone = "Europe/Lisbon";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = lib.mkDefault "us";
-    useXkbConfig = true; # use xkb.options in tty.
-  };
-
-  services.xserver = {
-    enable = true;
-    xkb = {
-      layout = "us";
-      options = "eurosign:e,caps:escape";
+{ inputs, ... }: let
+  pkgs = import inputs.nixpkgs {
+    system = "x86_64-linux";
+    config = {
+      allowUnfree = true;
     };
-
-    windowManager.bspwm.enable = true;
-    displayManager.lightdm.enable = true;
   };
+in
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+inputs.nixpkgs.lib.nixosSystem {
+  inherit pkgs;
 
-  services.libinput = {
-    enable = true;
-    touchpad.naturalScrolling = true;
-  };
+  system = "x86_64-linux";
+  specialArgs = { inherit inputs; };
 
-  environment.systemPackages = with pkgs; [
-    stow # try to replace with xdg.configFile in home-manager
+  modules = [
+    ./default-bak.nix
 
-    alsa-utils
-    brightnessctl
-    playerctl
-
-    bspwm
-
-    gcc
-    gdb
-    lua
-    gnumake
-    go
-    gopls
-
-    lightdm
-
-    mcontrolcenter # MSI Control Center
-
-    (polybar.override { pulseSupport = true; })
-
-    # valgrind
-    # (vscode)
-
-    # just to have `uptime --pretty`
-    procps
-
-    # gtk theme
-    dconf
+    inputs.home-manager.nixosModules.home-manager {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.users."afonso" = import ../../home/afonso;
+    }
   ];
-
-  programs.dconf.enable = true;
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  # not for steam but for useful options
-  programs.steam.enable = true;
-  programs.gamemode.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.05"; # Did you read the comment?
 }
