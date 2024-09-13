17d8ab2996fe5d3536991b0940298a8583a48cf7
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2024-09-13 01:58:45 +0100

    some improvements to mpv config

--- modules/common/mpv.nix
+++ modules/common/mpv.nix
@@ -12,8 +12,20 @@
         enable = true;
 
         config = {
+          gpu-api = "vulkan";
           hwdec = "auto-safe";
+          vo = "gpu-next";
           # hr-seek = "yes";
+
+          keep-open = "yes";
+        };
+
+        profiles = {
+          "protocol.http" = {
+            hls-bitrate = "max";
+            cache = "yes";
+            no-cache-pause = true;
+          };
         };
 
         bindings = {
