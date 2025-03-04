e77823de55b3833d38fe24eb56312fdcdc0e34c8
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2025-03-04 18:18:55 +0000

    added markdown rendering to nvim

---
+++ nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,19 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+  ft = { 'markdown' },
+  opts = {
+    code = {
+      sign = false,
+      width = 'block',
+      right_pad = 1,
+    },
+    heading = {
+      sign = false,
+      icons = {},
+    },
+    checkbox = {
+      custom = {},
+    },
+  },
+}
